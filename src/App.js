2620c4e2430463b9447558f97451d9370eb0f8ad
jtrenaud1s <jtrenaud1s@semo.edu>
2021-07-21 15:25:40 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,60 +1,105 @@
-import './css/App.css';
-import ExperienceItem from './components/ExperienceItem';
-import ResumeSection from './components/ResumeSection';
-import Sidebar from './components/Sidebar';
+import "./css/App.css";
+import ExperienceItem from "./components/ExperienceItem";
+import ResumeSection from "./components/ResumeSection";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div>
-    <div className="wrapper mt-lg-5">
-      <Sidebar>
-        <p>It me</p> 
-      </Sidebar>
-      <main className="content">
-        <ResumeSection title="About Me">
-          <p>I'm a developer and I do full stack stuffs</p>
-        </ResumeSection>
-        <ResumeSection title="Experience">
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-          <ExperienceItem position="Website Administrator" start="June 2020" end="Present" location="Cape Girardeau, Missouri">
-            <p>I do the thing with the semo website</p>
-          </ExperienceItem>
-        </ResumeSection>
-      </main>
+      <div className="wrapper mt-lg-5">
+        <Sidebar>
+          <p>It me</p>
+        </Sidebar>
+        <main className="content">
+          <ResumeSection title="About Me">
+            <p>I'm a developer and I do full stack stuffs</p>
+          </ResumeSection>
+          <ResumeSection title="Experience">
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri"
+            >
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri">
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri">
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri">
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri">
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri">
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri">
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri">
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri"
+            >
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri"
+            >
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+            <ExperienceItem
+              position="Website Administrator"
+              start="June 2020"
+              end="Present"
+              location="Cape Girardeau, Missouri"
+            >
+              <p>I do the thing with the semo website</p>
+            </ExperienceItem>
+          </ResumeSection>
+        </main>
       </div>
-      <footer className="footer">
-        fuck
-      </footer>
-
+      <footer className="footer">fuck</footer>
     </div>
   );
 }
