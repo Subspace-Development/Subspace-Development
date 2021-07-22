d035d7cf27a2cea315e63f7d988679b187dd0d3c
Jordan Renaud <jtrenaud1s@semo.edu>
2021-07-21 20:40:24 -0500

    fixed img error

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Profile(props) {
     return (
         <div className="profile">
-            {props.img && <img className="profile-image" src={props.img} />}
+            {props.img && <img className="profile-image" src={props.img} alt={props.name} />}
             <h1 className="name">{props.name}</h1>
             <h3 className="tagline">{props.tagline}</h3>
         </div>
